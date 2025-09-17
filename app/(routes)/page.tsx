5ebd5d118b1d2d63b76138bd5b2043d098d3a070
Serghei28 <serghei.petis@isa.utm.md>
2025-09-17 17:10:12 +0300

    Environment setup & featured products (Store)

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true});
+    const billboard = await getBillboard("a857cf64-dd38-4523-befe-c2a23db29b36");
+    
+    return(
+        <Container>
+            <div className="space-y pb-10">
+                <Billboard data={billboard} />
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
+            </div>
+        </Container>
+    )
+}
+
+export default HomePage;
